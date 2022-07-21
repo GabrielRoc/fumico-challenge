5e9595acb8ce8e9947e2b2c429182f896f5cc507
BuildTools <unconfigured@null.spigotmc.org>
2022-07-20 22:33:17 -0300

    run on env port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: process.env.CORS_WHITELIST.split(','),
+  });
+  await app.listen(process.env.APP_PORT, () =>
+    console.log(`Server started on port: ${process.env.APP_PORT}`),
+  );
 }
 bootstrap();
